7ee93f25c9d3b734a172d16b70775fb68c3e715a
antek314 <antoni.mielcarek@student.put.poznan.pl>
2025-06-11 11:01:25 +0200

    Ostatecznie, animacja wyrazow Mateusza, wyswietlanie dwoch rzedow, podzial plikow, hello world

--- pov.ino
+++ pov.ino
@@ -1,4 +1,6 @@
 #include "znaki.h"
+#include "wyswietlanie.h"
+
 
 #include <Adafruit_NeoPixel.h>
 #include <string.h>
@@ -7,6 +9,8 @@
 #define BUTTON_PIN   11
 #define PIXEL_PIN    6
 #define PIXEL_COUNT 16
+
+int licznik =  0;
 
 // Parameter 1 = number of pixels in strip,  neopixel stick has 8
 // Parameter 2 = pin number (most are valid)
@@ -25,59 +29,53 @@ void setup() {
 }
 
 void loop() {
-  startShow(0);
-}
-
-void startShow(int i) {
-  if(analogRead(A6) < 1005){
-  Funkcja(0.5);
+    if(analogRead(A6) < 400){
+      licznik++;
+      if(licznik >= 40 )
+      {
+        licznik = 0;
+      }
+    //FunkcjaAnimacja(0.5, licznik);
+    Funkcja(0.5, "hello", "world");
   }
 }
 
-void Funkcja(uint8_t wait)
+void Funkcja(uint8_t wait, char slowo_pierwsze[], char slowo_drugie[])
 {
-  uint32_t c = strip.Color(255, 255, 0);
-  char slowo_pierwsze[] = "lokomotywa";
-  char slowo_drugie[] = "bomba";
-  for(int g = 0; g < strlen(slowo_pierwsze); g++)
+  uint32_t c = strip.Color(250, 0, 0);
+  for(int g = 0; g < strlen(slowo_drugie); g++)
   {
     for(int j=1; j<7; j++) 
     {
       for(int i=0; i<strip.numPixels(); i++) 
       {
-        if(i>=8){Napisz(c, wait, slowo_pierwsze[g], i, j);}
-        if(i<=7) {Napisz(c, wait, slowo_drugie[g], i, j);}
+        if(i>8){Napisz(c, wait, slowo_pierwsze[g], i, j);}
+        if((i<8) && (i>0)) {Napisz(c, wait, slowo_drugie[g], i, j);}
       }
       strip.show();
       delay(wait);
     }
   }
 }
-void Napisz(uint32_t c, uint8_t wait, char znak, int i, int j)
+
+void FunkcjaAnimacja(uint8_t wait, int licznik)
 {
-  if(znak == ' '){Napisz_Spacje(i , j, c, wait);}
-  if(znak == 'a'){Napisz_A(i , j, c, wait);}
-  if(znak == 'b'){Napisz_B(i , j, c, wait);}
-  if(znak == 'c'){Napisz_C(i , j, c, wait);}
-  if(znak == 'd'){Napisz_D(i , j, c, wait);}
-  if(znak == 'e'){Napisz_E(i , j, c, wait);}
-  if(znak == 'f'){Napisz_F(i , j, c, wait);}
-  if(znak == 'g'){Napisz_G(i , j, c, wait);}
-  if(znak == 'h'){Napisz_H(i , j, c, wait);}
-  if(znak == 'i'){Napisz_I(i , j, c, wait);}
-  if(znak == 'j'){Napisz_J(i , j, c, wait);}
-  if(znak == 'k'){Napisz_K(i , j, c, wait);}
-  if(znak == 'l'){Napisz_L(i , j, c, wait);}
-  if(znak == 'm'){Napisz_M(i , j, c, wait);}
-  if(znak == 'n'){Napisz_N(i , j, c, wait);}
-  if(znak == 'o'){Napisz_O(i , j, c, wait);}
-  if(znak == 'p'){Napisz_P(i , j, c, wait);}
-  if(znak == 'r'){Napisz_R(i , j, c, wait);}
-  if(znak == 's'){Napisz_S(i , j, c, wait);}
-  if(znak == 't'){Napisz_T(i , j, c, wait);}
-  if(znak == 'u'){Napisz_U(i , j, c, wait);}
-  if(znak == 'w'){Napisz_W(i , j, c, wait);}
-  if(znak == 'y'){Napisz_Y(i , j, c, wait);}
-  if(znak == 'z'){Napisz_Z(i , j, c, wait);}
+  if(licznik < 10)
+  {
+    Funkcja(0.5,  "o", "rrrr");
+  }
+  if(licznik > 10 && licznik < 20)
+  {
+    Funkcja(0.5, " o", "pppp");
+  }
+  if(licznik > 20 && licznik < 30)
+  {
+    Funkcja(0.5,  "  o", "rrrr");
+  }
+  if(licznik > 30 && licznik < 40)
+  {
+    Funkcja(0.5,  "   o", "pppp");
+  }
+
 }
 
---
+++ wyswietlanie.h
@@ -0,0 +1,58 @@
+#include "znaki.h"
+#include <string.h>
+
+
+void Napisz(uint32_t c, uint8_t wait, char znak, int i, int j)
+{
+  if(znak == ' '){Napisz_Spacje(i , j, c, wait);}
+  if(znak == 'a'){Napisz_A(i , j, c, wait);}
+  if(znak == 'b'){Napisz_B(i , j, c, wait);}
+  if(znak == 'c'){Napisz_C(i , j, c, wait);}
+  if(znak == 'd'){Napisz_D(i , j, c, wait);}
+  if(znak == 'e'){Napisz_E(i , j, c, wait);}
+  if(znak == 'f'){Napisz_F(i , j, c, wait);}
+  if(znak == 'g'){Napisz_G(i , j, c, wait);}
+  if(znak == 'h'){Napisz_H(i , j, c, wait);}
+  if(znak == 'i'){Napisz_I(i , j, c, wait);}
+  if(znak == 'j'){Napisz_J(i , j, c, wait);}
+  if(znak == 'k'){Napisz_K(i , j, c, wait);}
+  if(znak == 'l'){Napisz_L(i , j, c, wait);}
+  if(znak == 'm'){Napisz_M(i , j, c, wait);}
+  if(znak == 'n'){Napisz_N(i , j, c, wait);}
+  if(znak == 'o'){Napisz_O(i , j, c, wait);}
+  if(znak == 'p'){Napisz_P(i , j, c, wait);}
+  if(znak == 'r'){Napisz_R(i , j, c, wait);}
+  if(znak == 's'){Napisz_S(i , j, c, wait);}
+  if(znak == 't'){Napisz_T(i , j, c, wait);}
+  if(znak == 'u'){Napisz_U(i , j, c, wait);}
+  if(znak == 'w'){Napisz_W(i , j, c, wait);}
+  if(znak == 'y'){Napisz_Y(i , j, c, wait);}
+  if(znak == 'z'){Napisz_Z(i , j, c, wait);}
+}
+
+
+void AnimacjaUsmiech(uint32_t kolor, uint8_t wait, int g, int i, int j)
+{
+  // Zakładamy: i = 0–15 (pion), j = 1–6 (kolumna), g = 0–4 (klatka)
+  
+  // Oczy - zmieniają się co klatkę (mruganie)
+  if ((j == 2 && (i == 5 || i == 10)) || (j == 4 && (i == 5 || i == 10))) {
+    if (g % 2 == 0) {
+      strip.setPixelColor(i, kolor); // oczy otwarte
+    } else {
+      strip.setPixelColor(i, 0); // oczy zamknięte (czarne)
+    }
+  }
+
+  // Uśmiech - porusza się w prawo i lewo z klatki na klatkę
+  if ((j == 3 || j == 4) && i == (11 + (g % 2))) {
+    strip.setPixelColor(i, kolor);
+  }
+
+  // Policzki - migają na końcach
+  if ((j == 1 && i == 7) || (j == 6 && i == 8)) {
+    if (g % 2 == 1) {
+      strip.setPixelColor(i, strip.Color(255, 0, 0)); // czerwone policzki
+    }
+  }
+}
